c413f8f297ee0f48a4537058c16e027d93c6849d
JIHUNJO123 <hoonijo@gmail.com>
2025-12-21 15:42:02 +0900

    Restore l10n generated files

---
+++ lib/l10n/generated/app_localizations_hi.dart
@@ -0,0 +1,249 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class AppLocalizationsHi extends AppLocalizations {
+  AppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get appTitle => 'SAT शबदवल मसटर';
+
+  @override
+  String get todayWord => 'आज क शबद';
+
+  @override
+  String get learning => 'सखन';
+
+  @override
+  String get levelLearning => 'कठनई सतर';
+
+  @override
+  String get allWords => 'सभ शबद';
+
+  @override
+  String get viewAllWords => 'सभ शबदवल दख';
+
+  @override
+  String get favorites => 'पसदद';
+
+  @override
+  String get savedWords => 'सहज गए शबद';
+
+  @override
+  String get flashcard => 'फलशकरड';
+
+  @override
+  String get cardLearning => 'करड लरनग';
+
+  @override
+  String get quiz => 'कवज';
+
+  @override
+  String get testYourself => 'अपन परकष ल';
+
+  @override
+  String get settings => 'सटगस';
+
+  @override
+  String get language => 'भष';
+
+  @override
+  String get displayLanguage => 'परदरशन भष';
+
+  @override
+  String get selectLanguage => 'भष चन';
+
+  @override
+  String get display => 'परदरशन';
+
+  @override
+  String get darkMode => 'डरक मड';
+
+  @override
+  String get fontSize => 'फनट आकर';
+
+  @override
+  String get notifications => 'सचनए';
+
+  @override
+  String get dailyReminder => 'दनक रमइडर';
+
+  @override
+  String get dailyReminderDesc => 'हर दन अधययन क यद दलए';
+
+  @override
+  String get removeAds => 'वजञपन हटए';
+
+  @override
+  String get adsRemoved => 'वजञपन हटए गए';
+
+  @override
+  String get thankYou => 'आपक समरथन क लए धनयवद!';
+
+  @override
+  String get buy => 'खरद';
+
+  @override
+  String get restorePurchase => 'खरदर पनरसथपत कर';
+
+  @override
+  String get restoring => 'पनरसथपत ह रह ह...';
+
+  @override
+  String get purchaseSuccess => 'खरदर सफल!';
+
+  @override
+  String get loading => 'लड ह रह ह...';
+
+  @override
+  String get notAvailable => 'उपलबध नह';
+
+  @override
+  String get info => 'जनकर';
+
+  @override
+  String get version => 'ससकरण';
+
+  @override
+  String get disclaimer => 'असवकरण';
+
+  @override
+  String get disclaimerText => 'यह ऐप एक सवततर SAT तयर उपकरण ह और ETS स सबदध, समरथत य सवकत नह ह';
+
+  @override
+  String get privacyPolicy => 'गपनयत नत';
+
+  @override
+  String get cannotLoadWords => 'शबद लड नह ह सकत';
+
+  @override
+  String get noFavoritesYet => 'अभ कई पसदद नह';
+
+  @override
+  String get tapHeartToSave => 'शबद सहजन क लए हरट आइकन टप कर';
+
+  @override
+  String get addedToFavorites => 'पसदद म जड गय';
+
+  @override
+  String get removedFromFavorites => 'पसदद स हटय गय';
+
+  @override
+  String get wordDetail => 'शबद ववरण';
+
+  @override
+  String get definition => 'परभष';
+
+  @override
+  String get example => 'उदहरण';
+
+  @override
+  String levelWords(String level) {
+    return '$level शबद';
+  }
+
+  @override
+  String get basic => 'बनयद';
+
+  @override
+  String get basicDesc => 'आधरभत शबद - 500 शबद';
+
+  @override
+  String get common => 'समनय';
+
+  @override
+  String get commonDesc => 'बर-बर परकषत - 1,000 शबद';
+
+  @override
+  String get advanced => 'उननत';
+
+  @override
+  String get advancedDesc => 'उचच-आवतत SAT - 800 शबद';
+
+  @override
+  String get expert => 'वशषजञ';
+
+  @override
+  String get expertDesc => 'चनतपरण शबदवल - 500 शबद';
+
+  @override
+  String get alphabetical => 'वरणमल करम';
+
+  @override
+  String get random => 'यदचछक';
+
+  @override
+  String get tapToFlip => 'पलटन क लए टप कर';
+
+  @override
+  String get previous => 'पछल';
+
+  @override
+  String get next => 'अगल';
+
+  @override
+  String get question => 'परशन';
+
+  @override
+  String get score => 'सकर';
+
+  @override
+  String get quizComplete => 'कवज परण!';
+
+  @override
+  String get finish => 'समपत';
+
+  @override
+  String get tryAgain => 'पन परयस कर';
+
+  @override
+  String get showResult => 'परणम दखए';
+
+  @override
+  String get wordToMeaning => 'शबद  अरथ';
+
+  @override
+  String get meaningToWord => 'अरथ  शबद';
+
+  @override
+  String get excellent => 'उतकषट! परण अक!';
+
+  @override
+  String get great => 'बहत अचछ! जर रख!';
+
+  @override
+  String get good => 'अचछ परयस! अभयस जर रख!';
+
+  @override
+  String get keepPracticing => 'अभयस जर रख! आप सधर करग!';
+
+  @override
+  String get levelA1 => 'शरआत';
+
+  @override
+  String get levelA2 => 'परथमक';
+
+  @override
+  String get levelB1 => 'मधयवरत';
+
+  @override
+  String get levelB2 => 'उचच मधयवरत';
+
+  @override
+  String get levelC1 => 'उननत';
+
+  @override
+  String get privacyPolicyContent => 'यह ऐप कई वयकतगत जनकर एकतर, सगरहत य सझ नह करत ह आपक परगत और पसदद कवल आपक डवइस पर सगरहत ह';
+
+  @override
+  String get restorePurchaseDesc => 'यद आपन पहल कस अनय डवइस पर वजञपन हटन क खरदर क ह, त यह टप करक पनरसथपत कर';
+
+  @override
+  String get restoreComplete => 'पनरसथपन परण';
+
+  @override
+  String get noPurchaseFound => 'कई पछल खरदर नह मल';
+}
